a87bd4b343c5b75acfe0bb8a43b9368301de9d1b
dfederspiel <dfederspiel@federnet.com>
2013-06-06 09:01:20 -0400

    Initializing Repository

---
+++ RavenDBCRUD/RDocumentStore.cs
@@ -0,0 +1,29 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RavenDBCRUD
+{
+    public class RDocumentStore
+    {
+        public IDocumentStore Store
+        {
+            get { return LazyDocStore.Value; }
+        }
+
+        private static readonly Lazy<IDocumentStore> LazyDocStore = new Lazy<IDocumentStore>(() =>
+        {
+            var docStore = new EmbeddableDocumentStore
+            {
+                DataDirectory = "Data"
+            };
+
+            docStore.Initialize();
+            return docStore;
+        });
+    }
+}
---
+++ RavenDBCrudTests/ContactTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RavenDBCRUD;
+using Raven.Client;
+using System.Linq;
+
+namespace RavenDBCrudTests
+{
+    [TestClass]
+    public class ContactTests : RDocumentStore
+    {
+        [TestMethod]
+        public void CreateContact()
+        {
+            var contact = new Contact()
+            {
+                Name = "John",
+                Age = 27
+            };
+            using (IDocumentSession session = Store.OpenSession())
+            {
+                session.Store(contact);
+                session.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void LoadContacts()
+        {
+            using (IDocumentSession session = Store.OpenSession())
+            {
+                var contacts =
+                (
+                    from c in session.Query<Contact>()
+                    select c
+                ).ToArray();
+
+                Assert.IsTrue(contacts != null);
+            }
+        }
+    }
+}
